1ff9211a10cce52f920701b8ff42ac53883ce607
Rohitrajak1807 <rrajak@platform9.com>
2023-04-14 08:51:00 +0000

    teamcity build changes,generated files

--- make.sh
+++ make.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
-pushd app-policy || exit
+pushd ../app-policy || exit
 make image || exit
 popd || exit
 
-pushd calicoctl || exit
+pushd ../calicoctl || exit
 make image || exit
 popd || exit
 
-pushd cni-plugin || exit
+pushd ../cni-plugin || exit
 make image || exit
 popd || exit
 
-pushd felix || exit
+pushd ../felix || exit
 make image || exit
 popd || exit
 
-pushd kube-controllers || exit
+pushd ../kube-controllers || exit
 make image || exit
 popd || exit
 
-pushd node || exit
+pushd ../node || exit
 make image || exit
 popd || exit
 
-pushd pod2daemon || exit
+pushd ../pod2daemon || exit
 make image || exit
 popd || exit
 
-pushd typha || exit
+pushd ../typha || exit
 make image || exit
 popd || exit
